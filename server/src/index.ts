023ece43916ad4857342364298f890c0f793faf7
binu-baiju <binubaiju000@gmail.com>
2023-05-12 12:43:43 +0530

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+const app = express();
+const mongoose = require('mongoose');
+
+import {config} from 'dotenv'
+config();
+
+const PORT = 5000;
+
+app.get("/",(req:Request,res:Response)=>{
+    res.send("Hello")
+});
+
+mongoose.connect(process.env.MONGO_URL!)
+    .then(()=>{
+        console.log(`Listening on port ${PORT} `);
+        
+    })
+
+
+app.listen(PORT);
