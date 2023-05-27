f291addb40c11f18b6ce1010faa457902001d76d
binu-baiju <binubaiju000@gmail.com>
2023-05-28 00:49:59 +0530

    form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home";
 import Dashboard from "./pages/Dashboard";
+import Form from "./pages/form";
+
 import Login from "./pages/login";
 
 export default function App() {
@@ -12,6 +14,8 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route  path="/login/*" element={<Login />} />
+        <Route  path="/form/*" element={<Form />} />
+
       </Routes>
     </Router>
   );
