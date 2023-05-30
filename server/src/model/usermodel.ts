d903d6a531e7e5657cf928d91fe1f84beab542a3
binu-baiju <binubaiju000@gmail.com>
2023-05-30 23:56:26 +0530

    Created model for dev and user

--- server/src/model/usermodel.ts
+++ server/src/model/usermodel.ts
@@ -4,12 +4,15 @@ const User = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    name: { type: String },
+    profession: { type: String },
+    devs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Dev" }],
   },
   {
     collection: "professional-data",
   }
 );
 
-const model = mongoose.model("ProfessionalsData", User);
+const userModel = mongoose.model("ProfessionalsData", User);
 
-export default model;
+export default userModel;
